6055ee05f3c2d17ebc04fd19d793109f80941723
oksana markiv <markiv_o@qwasar.io>
2021-05-03 16:16:02 +0000

    My Mult

--- ex05/my_mult.js
+++ ex05/my_mult.js
@@ -2,6 +2,6 @@ function my_mult(a, b) {
 let res = a * b
 return res
 };
-// consol.log(my_sub(0));
-// consol.log(my_sub(100));
-// consol.log(my_sub(-100));
+// consol.log(my_mult(0));
+// consol.log(my_mult(100));
+// consol.log(my_mult(-100));
